dc2cfea93df0c2497a2360fbd40caf25739cfe02
Felipe Pritsch Fahrion <felipefahrion@MacBook-Pro-de-Felipe-2.local>
2022-09-30 23:21:23 -0300

    fix issue

--- p2pPeer.java
+++ p2pPeer.java
@@ -7,7 +7,7 @@ public class p2pPeer {
 	public static void main(String[] args) throws IOException {
 		String vars[] = args[1].split("\\s");
 
-		Map<String, String> resourceList = new Peer(null, null, null, vars[2]).getResourceList()
+		Map<String, String> resourceList = new Peer(null, null, null, vars[2]).getResourceList();
 
 		new p2pPeerThread(args, resourceList).start();
 		new p2pPeerHeartbeat(args).start();
